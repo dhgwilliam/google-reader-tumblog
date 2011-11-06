cb8072de52ef7383b7537907e85b437da259f2ad
David Gwilliam <dhgwilliam@gmail.com>
2011-11-06 12:42:24 -0500

    include original parse script and my reader json file

---
+++ parse.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'redis'
+
+redis = Redis.new
+
+google_string = String.new
+
+google_snippet = File.readlines("/home/david/reader.json")
+google_snippet.each { |lines| google_string = google_string.chomp + lines.chomp }
+google_json = JSON.parse(google_string)
+item_array = google_json[google_json.keys[0]]
+item_array.each do |item|
+  item_id = item.values_at("object")[0].values_at("id")
+  redis.set item_id[0], item.to_json
+end
+redis_test = redis.get "tag:google.com,2005:reader/item/b4b4d935e2ce43a6"
+puts JSON.parse(redis_test)
