d9c31e3cb12853d617f7e08480a87a8393df27ee
David G <dhgwilliam@gmail.com>
2011-11-06 16:58:38 -0500

    updated parse and index to work with timestamped keys

--- parse.rb
+++ parse.rb
@@ -14,8 +14,8 @@ google_snippet.each { |lines| google_string = google_string.chomp + lines.chomp 
 google_json = JSON.parse(google_string)
 item_array = google_json.values_at("items")[0]
 item_array.each do |item|
-  item_id = item.values_at("id")[0]
+  item_id = "#{item.values_at("crawlTimeMsec").slice(0..9)[0]}:#{item.values_at("id")[0]}"
   redis.set item_id, item.to_json
 end
-redis_test = redis.get "tag:google.com,2005:reader/item/0e5c79d3a27b42d4"
+redis_test = redis.get redis.keys("*tag:google.com,2005:reader/item/0e5c79d3a27b42d4")[0]
 puts JSON.parse(redis_test).values_at("id")
