7f4be98bba274c01656eb812dc11fdcb47b23a59
David Gwilliam <dhgwilliam@gmail.com>
2011-11-06 17:37:05 -0500

    added get_article helper, moved nav to top

--- index.rb
+++ index.rb
@@ -9,6 +9,15 @@ before do
 end
 
 get '/' do
+  @object = get_article((redis.keys("*reader/item/*").sort { |x,y| y <=> x }.take(1)[0]))
+  @item = @object.values_at("item")[0]
+  @title = @object.values_at("title")[0]
+  @url = @object.values_at("url")[0]
+  @id = @object.values_at("id")[0]
+  haml :index    
+end
+
+get '/random' do
   @object = JSON.parse(redis.get(redis.randomkey))
   if @object.has_key?("content")
     @item = @object.values_at("content")[0].values_at("content")[0]
@@ -38,3 +47,21 @@ get "/css/:stylesheet.css" do
   content_type "text/css", :charset => "UTF-8"
   sass :"css/#{params[:stylesheet]}"
 end 
+
+helpers do
+  def get_article(key)
+    redis = Redis.new
+    redis.select 1
+    @object = JSON.parse(redis.get("#{key}"))
+    if @object.has_key?("content")
+      @item = @object.values_at("content")[0].values_at("content")[0]
+    elsif @object.has_key?("summary")
+      @item = @object.values_at("summary")[0].values_at("content")[0]
+    end  
+    @title = @object.values_at("title")[0]
+    @url = @object.values_at("alternate")[0][0].values_at("href")[0]
+    @id = @object.values_at("id")[0].scan(/[a-zA-Z0-9]+$/)[0]
+    return { "item" => @item, "title" => @title, "url" => @url, "id" => @id}
+  end
+
+end
