5a36eb7031e5d72d7e03cc15c5903799f13a45ae
David Gwilliam <dhgwilliam@gmail.com>
2011-11-06 12:39:45 -0500

    initial commit

---
+++ config.ru
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'sinatra'
+require 'bundler'
+require 'redis'
+require './index'
+
+root_dir = File.dirname(__FILE__)
+ 
+set :environment, ENV['RACK_ENV'].to_sym
+set :root,        root_dir
+set :app_file,    File.join(root_dir, 'index.rb')
+disable :run
+
+run Sinatra::Application
---
+++ index.rb
@@ -0,0 +1,20 @@
+require 'json'
+
+redis = Redis.new
+
+before do 
+  # Strip the last / from the path
+  request.env['PATH_INFO'].gsub!(/\/$/, '')
+end
+
+get '/' do
+  @object = JSON.parse(redis.get(redis.randomkey))
+  @item = @object.values_at("object")[0].values_at("content")[0]
+  @title = @object.values_at("object")[0].values_at("displayName")[0]
+  haml :index    
+end
+
+get "/css/:stylesheet.css" do
+  content_type "text/css", :charset => "UTF-8"
+  sass :"css/#{params[:stylesheet]}"
+end 
